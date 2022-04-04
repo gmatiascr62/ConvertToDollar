c90703b5e566bec896b6bcb53fc3ec281266264b
German Matias <gmatiascr62@gmail.com>
2022-04-04 08:52:59 -0300

    subiendo el primer archivo

---
+++ convert_float_dollar.py
@@ -0,0 +1,39 @@
+
+
+class ConvertToDollar():
+	
+	@classmethod
+	def dolarizar(self, number) -> str:
+		numero=number
+		numero=round(numero,2)
+		numero=str(numero)
+		numeroFormateado=""
+		if numero[-2] == ".":
+			numero=numero+"0"
+		insertar=len(numero)-7
+		for i in numero:
+			if i == ".":
+				i=","
+			numeroFormateado+=i
+		numero=numeroFormateado
+		numeroFormateado=""
+		while insertar > -1:
+			contador=0
+			for i in numero:
+				if str(contador) == str(insertar):
+					i=i+"."
+					insertar=insertar-3
+				numeroFormateado+=i
+				contador+=1
+			numero=numeroFormateado
+			numeroFormateado=""
+		resultadousd = numero
+		return f'$ {resultadousd}'
+	
+print(ConvertToDollar.dolarizar(458776.763))
+
+
+
+
+		
+		
